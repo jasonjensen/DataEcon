df9e3c12ac52481d27f0ae0b69aea8c5ef09c598
Jason Jensen <jason.jensen@hey.com>
2025-09-13 13:38:54 -0400

    matlab: write scalars

--- matlab/private/daecenums.m
+++ matlab/private/daecenums.m
@@ -8,6 +8,7 @@ if isempty(enums)
     enums.type_t=struct('type_none',0,'type_integer',1,'type_signed',1,'type_unsigned',2,'type_date',3,'type_float',4,'type_complex',5,'type_string',6,'type_other_scalar',7,'type_vector',10,'type_range',11,'type_tseries',12,'type_other_1d',13,'type_matrix',20,'type_mvtseries',21,'type_other_2d',22,'type_tensor',30,'type_ndtseries',31,'type_other_nd',32,'type_any',-1);
     enums.class_t=struct('class_catalog',0,'class_scalar',1,'class_vector',2,'class_tseries',2,'class_matrix',3,'class_mvtseries',3,'class_tensor',4,'class_ndtseries',4,'class_any',-1);
     enums.status_t=struct('DE_SUCCESS',0,'DE_ERR_ALLOC',-1000,'DE_BAD_AXIS_TYPE',-999,'DE_BAD_NUM_AXES',-998,'DE_BAD_CLASS',-997,'DE_BAD_TYPE',-996,'DE_BAD_ELTYPE',-995,'DE_BAD_ELTYPE_NONE',-994,'DE_BAD_ELTYPE_DATE',-993,'DE_BAD_NAME',-992,'DE_BAD_FREQ',-991,'DE_SHORT_BUF',-990,'DE_OBJ_DNE',-989,'DE_AXIS_DNE',-988,'DE_ARG',-987,'DE_NO_OBJ',-986,'DE_EXISTS',-985,'DE_BAD_OBJ',-984,'DE_NULL',-983,'DE_DEL_ROOT',-982,'DE_MIS_ATTR',-981,'DE_INEXACT',-980,'DE_RANGE',-979,'DE_INTERNAL',-978);
+    enums.type_t_by_matlab_class=struct('int32',1,'int64',1,'logical',2,'uint32',2,'uint64',2,'datetime',3,'double',4,'char',6,'string',6);
 end
 
 enuminfo = enums;
